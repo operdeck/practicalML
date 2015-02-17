9ebbaf4492692f11d9b45a17c4247094a9c3e45c
Otto Perdeck <operdeck@gmail.com>
2015-02-17 08:42:03 +0100

    With some prediction

--- assignment.R
+++ assignment.R
@@ -5,3 +5,8 @@ train <- read.csv("pml-training.csv", sep=",", quote="\"")
 test <- read.csv("pml-testing.csv", sep=",", quote="\"")
 
 model <- train(classe ~ ., method="rpart", data=train)
+
+# explore
+pred <- predict(model, test)
+confusionMatrix(pred, test$classe)
+# etc, see http://www.edii.uclm.es/~useR-2013/Tutorials/kuhn/user_caret_2up.pdf
