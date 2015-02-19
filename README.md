b360e874a6b98f01645f11432a66aa8e56e69a39
Otto Perdeck <perdo@pega.com>
2015-02-19 15:41:24 +0100

    First shot

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # practicalML
 Practical Machine Learning - Coursera John Hopkins
+
+Plan
+
+1. Get train and test data
+2. Divide train data into train/validate
+3. Clean up the train data
+4. Build model
+5. Use validate data for cross validation --> gives out of sample error estimate
+6. Apply on test data to predict outcomes
+
+Ad 3:
+Many cols have loads of missing data. Using exploratory data analysis we've selected
+only the ones that do not have many NA / missing values.
+
+Row 15350 is strange: replace ,,A, with ,NA, so the nr of columns matches in that row
+
+
+Ad 4:
+Creating a simple tree model. Since the outcome is a categorical variable, it makes
+more sense to create a classification tree.
+
--- assignment.R
+++ assignment.R
@@ -1,12 +1,54 @@
 library(caret)
 library(ggplot2)
 
-train <- read.csv("pml-training.csv", sep=",", quote="\"")
-test <- read.csv("pml-testing.csv", sep=",", quote="\"")
+# Read the datasets. Outside of R, I've cleaned them by removing the quote characters and 
+# changed the Excel DIV/0 entries to NA.
+sampleset <- read.csv("pml-training.csv", sep=",")
+# special treatment for columns that it thinks are factors
+sampleset$X <- as.numeric(sampleset$X)
+testset <- read.csv("pml-testing.csv", sep=",",colClasses=lapply(sampleset,class))
 
-model <- train(classe ~ ., method="rpart", data=train)
+# Split train set into train and validation
+inTrain <- createDataPartition(y=sampleset$classe,p=0.6,list=F)
+trainset <- sampleset[inTrain,]
+print("Omitting rows and cols for training set:")
+print(dim(trainset))
+trainset <- subset(trainset, select = c(new_window,num_window,roll_belt,pitch_belt,yaw_belt,total_accel_belt,
+                                        gyros_belt_x,gyros_belt_y,gyros_belt_z,accel_belt_x,accel_belt_y,accel_belt_z,magnet_belt_x,magnet_belt_y,magnet_belt_z,roll_arm,pitch_arm,yaw_arm,total_accel_arm,
+                                        gyros_arm_x,gyros_arm_y,gyros_arm_z,accel_arm_x,accel_arm_y,accel_arm_z,magnet_arm_x,magnet_arm_y,magnet_arm_z,
+                                        roll_dumbbell,pitch_dumbbell,yaw_dumbbell,total_accel_dumbbell,
+                                        gyros_dumbbell_x,gyros_dumbbell_y,gyros_dumbbell_z,accel_dumbbell_x,accel_dumbbell_y,accel_dumbbell_z,magnet_dumbbell_x,magnet_dumbbell_y,magnet_dumbbell_z,roll_forearm,pitch_forearm,yaw_forearm,
+                                        gyros_forearm_x,gyros_forearm_y,gyros_forearm_z,accel_forearm_x,accel_forearm_y,accel_forearm_z,magnet_forearm_x,magnet_forearm_y,magnet_forearm_z,classe))
+# also leave out a few rows with NA's
+# perhaps we should consider knn preprocessing to infer the missing values instead
+trainset <- na.omit(trainset)
+print(dim(trainset))
+
+valset <- sampleset[-inTrain,]
+# same for the validation set
+print("Omitting rows and cols for validation set:")
+print(dim(valset))
+valset <- subset(valset, select = c(new_window,num_window,roll_belt,pitch_belt,yaw_belt,total_accel_belt,
+                                        gyros_belt_x,gyros_belt_y,gyros_belt_z,accel_belt_x,accel_belt_y,accel_belt_z,magnet_belt_x,magnet_belt_y,magnet_belt_z,roll_arm,pitch_arm,yaw_arm,total_accel_arm,
+                                        gyros_arm_x,gyros_arm_y,gyros_arm_z,accel_arm_x,accel_arm_y,accel_arm_z,magnet_arm_x,magnet_arm_y,magnet_arm_z,
+                                        roll_dumbbell,pitch_dumbbell,yaw_dumbbell,total_accel_dumbbell,
+                                        gyros_dumbbell_x,gyros_dumbbell_y,gyros_dumbbell_z,accel_dumbbell_x,accel_dumbbell_y,accel_dumbbell_z,magnet_dumbbell_x,magnet_dumbbell_y,magnet_dumbbell_z,roll_forearm,pitch_forearm,yaw_forearm,
+                                        gyros_forearm_x,gyros_forearm_y,gyros_forearm_z,accel_forearm_x,accel_forearm_y,accel_forearm_z,magnet_forearm_x,magnet_forearm_y,magnet_forearm_z,classe))
+valset <- na.omit(valset)
+print(dim(valset))
+
+# rpart for straightforward CART decision tree
+# try out treebag for trees with bagging or rf for rand forest
+
+model <- train(classe ~ ., method="treebag", data=trainset)
+preds <- predict(model, newdata=valset)
 
 # explore
-pred <- predict(model, test)
-confusionMatrix(pred, test$classe)
+print(table(preds, valset$classe))
+# number of times we're right - indication for out of sample error
+print(sum(preds == valset$classe) / length(preds))
+
 # etc, see http://www.edii.uclm.es/~useR-2013/Tutorials/kuhn/user_caret_2up.pdf
+
+# nice:
+# fancyRpartPlot(model$finalModel) from lib rattle (rpart.plot)
